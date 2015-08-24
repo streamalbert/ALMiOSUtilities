086b859d294dd98a48359ad77d0d8e5b3f2696cb
Weichuan Tian <stream.albert@gmail.com>
2015-08-23 22:17:50 -0700

    project creation

---
+++ ALMiOSUtilities/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  ALMiOSUtilities
+//
+//  Created by Weichuan Tian on 8/23/15.
+//  Copyright (c) 2015 Weichuan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ ALMiOSUtilities/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ALMiOSUtilities
+//
+//  Created by Weichuan Tian on 8/23/15.
+//  Copyright (c) 2015 Weichuan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
